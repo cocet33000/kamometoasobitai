f0eccfc9c65743c21553e4075b5ec7414c94bc44
cocet33000 <cocet33000@gmail.com>
2019-01-26 19:40:08 +0900

    none

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def karasu():
     data = request.data.decode('utf-8')
     data = json.loads(data)
     im = np.array(data)
-    print(type(im))
+    print(im.shape)
     pil_img = Image.fromarray(im)
     pil_img.save('save.png')
     print(pil_img)
